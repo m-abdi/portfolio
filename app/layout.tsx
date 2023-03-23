0758e2005737dda5d8cec816bc8546f3dce4c4be
Mehdi Abdi <m.abdi.public@gmail.com>
2023-03-23 13:11:15 +0330

    Move clarity code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import './globals.css';
 import './fonts.css';
 
-import Script from 'next/script';
-
 export const metadata = {
   title: 'Mehdi Abdi | Software Engineer',
   description: 'Portfolio and blog',
@@ -15,15 +13,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <Script>
-        {`<script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "gd0iiosh3x");
-</script>`}
-      </Script>
       <body>{children}</body>
     </html>
   );

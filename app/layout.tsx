200c493c0b11e638d9109829fc4e8e3f288fd497
Mehdi Abdi <m.abdi.public@gmail.com>
2023-03-23 13:28:13 +0330

    Add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import './globals.css';
 import './fonts.css';
 
+import Head from 'next/head';
+import Script from 'next/script';
+
 export const metadata = {
   title: 'Mehdi Abdi | Software Engineer',
   description: 'Portfolio and blog',
@@ -13,6 +16,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <Head>
+        <script
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=G-9RLJ6J75KN'
+        ></script>
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-9RLJ6J75KN');`}
+        </script>
+        <script>
+          {`<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "gd0iiosh3x");
+</script>`}
+        </script>
+      </Head>
       <body>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import Script from 'next/script';
 import TypewriterEffect from './components/TypewriterEffect/TypewriterEffect';
 
 const skills = [
@@ -46,15 +45,6 @@ const code = `function main() {<br>
 export default function Home() {
   return (
     <main className='min-h-[100vh] flex flex-col gap-8'>
-      <Script>
-        {`<script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "gd0iiosh3x");
-</script>`}
-      </Script>
       <div
         className='transition-[background-color] hover:bg-[whitesmoke] border-[1px] rounded-[50%] bg-[white] w-[80px] h-[80px] flex flex-col items-center justify-center shadow-xl border-solid sticky top-3 mx-auto z-10'
         style={{ transitionTimingFunction: 'ease-out' }}

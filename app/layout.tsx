f02bb6dcc915e7c294c1181f2ef2d9a574f44c42
Mehdi Abdi <m.abdi.public@gmail.com>
2024-08-29 15:30:30 +0330

    Replace google analytics with matomo

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,17 +32,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <Script
-        async
-        src='https://www.googletagmanager.com/gtag/js?id=G-9RLJ6J75KN'
-      ></Script>
-      <Script>
-        {`window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-9RLJ6J75KN');`}
-      </Script>
+     <Script id='matomo'>
+{`  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//matomo-e1ie-mahgd.liara.run/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '2']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();`}
+        </Script>
       <Script>
         {`<script type="text/javascript">
     (function(c,l,a,r,i,t,y){
